0b05862784aeb2b4fea492b0b3ea6256f809f8a4
Aaron Lun <lun.aaron@gene.com>
2022-10-06 14:59:44 -0700

    Fixed and documented the saving, annotation functions.

--- R/annotateObject.R
+++ R/annotateObject.R
@@ -1,9 +1,67 @@
+#' Add calcite annotation
+#'
+#' Add calcite's required annotation to a Bioconductor \linkS4class{Annotated} object.
+#'
+#' @param x An \linkS4class{Annotated} object.
+#' @param title String containing the object's title.
+#' @param description String containing the description of the object.
+#' @param maintainers List containing the identity of the maintainers.
+#' Each entry may be a \code{\link{person}}-formatted string, or a list containing the \code{name} and \code{email} strings.
+#' 
+#' Alternatively, a character vector of \code{\link{person}}-formatted strings.
+#' @param species An integer vector of NCBI taxonomy IDs for the species relevant to the data in \code{x}. 
+#' @param genome List describing the genomes involved in constructing \code{x}.
+#' Each entry should be a list containing:
+#' \itemize{
+#' \item \code{id}, a string containing the genome build ID (e.g., hg38, NCBIm37)
+#' \item \code{type}, a string specifying the type of ID (e.g., UCSC, Ensembl)
+#' }
+#' Check out \url{https://artifactdb.github.io/calcite-schemas/array/v1.html#allOf_i0_genome} for details.
+#' @param origin List describing the origin of \code{x}.
+#' Each entry should be a list containing:
+#' \itemize{
+#' \item \code{source}, a string describing the source repository, e.g., PubMed, GEO, ArrayExpress.
+#' \item \code{id}, a string containing an identifier within \code{source}.
+#' }
+#' Check out \url{https://artifactdb.github.io/calcite-schemas/array/v1.html#allOf_i0_origin} for details.
+#' @param annotation List of calcite-relevant metadata.
+#' 
+#' @return 
+#' For \code{annotateObject}, \code{x} is returned with extra fields in its \code{\link{metadata}}.
+#'
+#' For \code{objectAnnotation}, a list of calcite-relevant metadata is returned.
+#'
+#' For \code{setannotation}, \code{x} is returned after replacing the calcite-relevant metadata with \code{annotation}.
+#' 
+#' @examples
+#' library(S4Vectors)
+#' df <- DataFrame(X=1:10, Y=LETTERS[1:10], Z=factor(letters[1:10]))
+#'
+#' df <- annotateObject(df,
+#'     title="FOO",
+#'     description="I am a data frame",
+#'     maintainers="Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>",
+#'     species=9606,
+#'     genome=list(list(id="hg38", source="UCSC")),
+#'     origin=list(list(source="PubMed", id="123456789"))
+#' )
+#'
+#' anno <- objectAnnotation(df)
+#' str(anno)
+#'
+#' anno$maintainers <- c(anno$maintainers, 
+#'     list(list(name="Darth Vader", email="vader@empire.gov")))
+#' df <- setAnnotation(df, anno)
+#' 
+#' @author Aaron Lun
+#'
 #' @export
 #' @importFrom S4Vectors metadata<- metadata
 annotateObject <- function(x, title, description, maintainers, species, genome, origin) {
+    maintainers <- as.list(maintainers)
     for (m in seq_along(maintainers)) {
         if (is.character(maintainers[[m]])) {
-            frag <- as.person(m)
+            frag <- as.person(maintainers[[m]])
             maintainers[[m]] <- list(name = paste(frag$given, frag$family), email = frag$email)
         }
     }
@@ -14,10 +72,11 @@ annotateObject <- function(x, title, description, maintainers, species, genome, 
         maintainers=maintainers,
         species=species,
         genome=genome,
-        origin=origin
+        origin=origin,
+        bioc_version=as.character(BiocManager::version())
     )
 
-    setObjectAnnotation(x, meta)
+    setAnnotation(x, meta)
 }
 
 #' @export
--- R/saveObject.R
+++ R/saveObject.R
@@ -1,18 +1,80 @@
+#' Save an object to file
+#'
+#' Save an object to a staging directory in preparation for upload.
+#' Multiple objects may be saved inside the same staging directory,
+#' though users should avoid saving an object inside another object's subdirectory (created by a previous \code{saveObject} call).
+#'
+#' @param x A supported Bioconductor object, with annotation added by \code{\link{annotateObject}}.
+#' @param dir String containing a path to a staging directory.
+#' @param path String containing the relative path inside the staging directory in which to save the object's contents.
+#' This should not be nested inside any subdirectories created by previous \code{saveObject} calls.
+#'
+#' @return 
+#' \code{x} is saved to the specified location, and \code{NULL} is invisibly returned.
+#'
+#' @author Aaron Lun
+#'
+#' @examples
+#' # Making an exciting DataFrame.
+#' library(S4Vectors)
+#' df <- DataFrame(X=1:10, Y=LETTERS[1:10], Z=factor(letters[1:10]))
+#' df$AA <- DataFrame(foo=runif(10), bar=rnorm(10))
+#'
+#' df <- annotateObject(df,
+#'     title="FOO",
+#'     description="I am a data frame",
+#'     maintainers="LTLA <infinite.monkeys.with.keyboards@gmail.com>",
+#'     species=9606,
+#'     genome=list(list(id="hg38", source="UCSC")),
+#'     origin=list(list(source="PubMed", id="123456789"))
+#' )
+#'
+#' # Staging the DataFrame.
+#' tmp <- tempfile()
+#' dir.create(tmp)
+#' saveObject(df, tmp, "df1")
+#'
+#' list.files(tmp, recursive=TRUE)
+#'
+#' @seealso
+#' \code{\link{annotateObject}}, to add the mandatory annotation to all objects.
+#'
+#' \code{\link{uploadDirectory}}, to upload all objects to the calcite store.
+#' 
 #' @export
-#' @importFrom alabaster.base stageObject .writeMetadata
+#' @importFrom alabaster.base .altStageObject .writeMetadata .createRedirection
 #' @importFrom zircon uploadProject
-saveObject <- function(x, dir, name) {
+saveObject <- function(x, dir, path) {
+    olds <- .altStageObject(calciteStageObject)
+    on.exit(.altStageObject(olds), add=TRUE)
+    meta <- calciteStageObject(x, dir, path, child=FALSE)
+
     extras <- objectAnnotation(x)
-    x <- .wipe_annotation(x)
-
-    dir.create(dir, showWarnings=FALSE)
-    meta <- stageObject(x, dir, name)
-
     extras <- extras[setdiff(names(extras), "_extra")]
     meta <- c(meta, extras)
-    meta$organism <- I(meta$organism)
+    meta$species <- I(meta$species)
 
     resource <- .writeMetadata(meta, dir)
+    .writeMetadata(.createRedirection(dir, path, meta$path), dir)
 
-    # TODO: create redirection here.
+    invisible(NULL)
 }
+
+#' @import methods
+setGeneric("calciteStageObject", function(x, dir, path, child=FALSE, ...) standardGeneric("calciteStageObject"))
+
+#' @importFrom alabaster.base stageObject 
+setMethod("calciteStageObject", "ANY", function(x, dir, path, child=FALSE, ...) {
+    meta <- stageObject(x, dir, path, child=child, ...)
+    attr(meta[["$schema"]], "package") <- "calcite.schemas"
+    meta 
+})
+
+#' @importFrom S4Vectors metadata<- metadata
+setMethod("calciteStageObject", "Annotated", function(x, dir, path, child=FALSE, ...) {
+    # Avoid staging the internal metadata.
+    all.ints <- names(metadata(x)) == ".internal"
+    metadata(x) <- metadata(x)[!all.ints]
+    callNextMethod()
+})
+
