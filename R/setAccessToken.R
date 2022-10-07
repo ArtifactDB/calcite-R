45eea2bff68b06602759cd79861b347acdf6c700
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2022-10-06 23:59:43 -0700

    Got the uploading function to work. (#1)
    This includes support for authentication via GitHub. Some effort is made to
    cache the token for re-use in the same or different R sessions, as well as some
    exported functions for token management.
    
    Added a new getter/setter for the REST URL. Updated the REST URL to the new
    calcite API, and got a simple example to work.

--- R/setAccessToken.R
+++ R/setAccessToken.R
@@ -1,12 +1,45 @@
 auth.globals <- new.env()
 auth.globals$info <- NULL
 
+#' Get or set the GitHub access token
+#'
+#' Pretty much as the title says.
+#' calcite uses GitHub for user authentication.
+#'
+#' @param token String containing a GitHub personal access token.
+#' If missing, the user will be prompted to supply a token.
+#' If \code{NULL}, any existing tokens are cleared.
+#' @param cache Logical scalar indicating whether the token should be cached to file for re-use in other R sessions.
+#' If \code{FALSE}, the token is only kept in memory for the current session.
+#' @param prompt Logical scalar indicating whether the user should be prompted to supply token details if no cached token exists.
+#'
+#' @return 
+#' For \code{setAccessToken}, any non-\code{NULL} \code{token} (or its interactively supplied counterpart) will be cached in memory and/or on file, depending on \code{cache}.
+#' A list is invisibly returned containing details about the token including its value, the corresponding user account and the expiry time.
+#' If \code{token=NULL}, any cached token is cleared from file and memory, and \code{NULL} is invisibly returned.
+#'
+#' For \code{accessTokenInfo}, a list is returned containing the token details.
+#' If \code{prompt=FALSE} and no cached token is present, \code{NULL} is returned.
+#'
+#' @author Aaron Lun
+#'
+#' @examples
+#' \dontrun{setAccessToken()}
+#'
+#' accessTokenInfo(FALSE)
+#'
 #' @export
 #' @importFrom httr GET add_headers content
-login <- function(token) {
+setAccessToken <- function(token, cache=TRUE) {
+    token.path <- .token_cache_path()
+    if (!missing(token) && is.null(token)) {
+        auth.globals$info <- NULL
+        unlink(token.path)
+        return(invisible(NULL))
+    }
+
     expiry <- NULL
     name <- NULL
-
     if (missing(token)) {
         token <- getPass("Please generate a new Github personal access token.
 
@@ -32,7 +65,7 @@ TOKEN: ")
             stop("empty token supplied")
         }
 
-    } else {
+    } else if (!is.null(token)) {
         res <- GET("https://api.github.com/user", add_headers(Authorization=paste("Bearer ", token)))
         if (res$status_code >= 300) {
             stop("failed to verify this token with GitHub (status code ", out$status_code, ")")
@@ -41,30 +74,36 @@ TOKEN: ")
         name <- content(res)$login
     }
 
-    writeLines(c(token, name, expiry), con=token.path)
+    dir.create(dirname(token.path), showWarnings=FALSE, recursive=TRUE)
+    if (cache) {
+        writeLines(c(token, name, expiry), con=token.path)
+    }
     vals <- list(token=token, name=name, expires=expiry)
     auth.globals$info <- vals
     invisible(vals)
 }
 
 #' @export
+#' @rdname setAccessToken
 accessTokenInfo <- function(prompt=interactive()) {
     vals <- auth.globals$info
+    token.path <- .token_cache_path()
 
+    rerun <- FALSE
     if (is.null(vals)) {
         if (!file.exists(token.path)) {
             rerun <- TRUE
         } else {
             lines <- readLines(token.path)
             vals <- list(token = lines[1], name = lines[2], expires = as.double(lines[3]))
-            if (vals$expires > as.double(Sys.time())) {
+            if (vals$expires <= as.double(Sys.time())) {
                 unlink(token.path)
                 auth.globals$info <- NULL
                 rerun <- TRUE
             }
         }
     } else {
-        if (vals$expires > as.double(Sys.time())) {
+        if (vals$expires <= as.double(Sys.time())) {
             unlink(token.path)
             auth.globals$info <- NULL
             rerun <- TRUE
@@ -73,7 +112,7 @@ accessTokenInfo <- function(prompt=interactive()) {
 
     if (rerun) {
         if (prompt) {
-            vals <- login()
+            vals <- setAccessToken()
         } else {
             return(NULL)
         }
@@ -81,7 +120,6 @@ accessTokenInfo <- function(prompt=interactive()) {
 
     vals
 }
-
 
 #' @importFrom httr headers
 .process_expiry <- function(res) {
@@ -94,13 +132,16 @@ accessTokenInfo <- function(prompt=interactive()) {
     }
 }
 
-#' @importFrom getPass getPass
 #' @importFrom tools R_user_dir
+.token_cache_path <- function() {
+    dir <- R_user_dir("calcite")
+    file.path(dir, "token.txt")
+}
+
+#' @importFrom getPass getPass
+#' @importFrom zircon identityAvailable identityHeaders
 #' @importFrom httr GET
 .setup_github_identities <- function() {
-    dir <- R_user_dir("calcite")
-    token.path <- file.path(dir, "token.txt")
-
     olda <- identityAvailable(function() !is.null(accessTokenInfo(prompt=FALSE)))
     oldh <- identityHeaders(function() list(Authorization=paste0("Bearer ", accessTokenInfo()$token)))
 
--- R/uploadDirectory.R
+++ R/uploadDirectory.R
@@ -1,6 +1,36 @@
+#' Upload a staging directory to the calcite store
+#' 
+#' Upload a staging directory to the calcite backend.
+#' The directory should contain objects saved with \code{\link{saveObject}}.
+#'
+#' @param dir String containing a path to a staging directory.
+#' @param project String containing the name of the project.
+#' @param version String containing the version of the project.
+#' @param owners Character vector of GitHub user names of project owners.
+#' Defaults to the currently authenticated user from \code{\link{setAccessToken}}.
+#' @param viewers Character vector of GitHub user names of allowed viewers.
+#' @param public Logical scalar indicating whether the project should be public.
+#' @param expires Integer scalar specifying the expiry date for this version of the project.
+#' If \code{NULL}, the uploaded version will not expire.
+#' @param collapse.md5.duplicates Logical scalar indicating whether duplicated files with the same MD5 checksum should be collapsed.
+#'
+#' @return \code{NULL} is invisibly returned on success.
+#'
+#' @author Aaron Lun
+#'
+#' @examples
+#' example(saveObject, echo=FALSE) # re-using this example.
+#'
+#' \dontrun{uploadDirectory(tmp, "FOO", "BAR", expires=1)}
+#'
+#' @seealso
+#' \code{\link{saveObject}}, to save objects to the staging directory.
+#'
+#' \code{\link{fetchObject}}, to fetch an object from the calcite backend. 
+#'
 #' @export
 #' @importFrom zircon uploadProject
-uploadDirectory <- function(dir, project, version, owners=NULL, viewers=NULL, public=TRUE) {
+uploadDirectory <- function(dir, project, version, owners=NULL, viewers=NULL, public=TRUE, expires=NULL, collapse.md5.duplicates=TRUE) {
     fun <- .setup_github_identities()
     on.exit(fun())
 
@@ -13,5 +43,12 @@ uploadDirectory <- function(dir, project, version, owners=NULL, viewers=NULL, pu
         read_access = if (public) "public" else "viewers" 
     )
 
-    uploadProject(dir, restURL(), project, version, permissions=permissions)
+    uploadProject(dir, 
+        url=restURL(), 
+        project=project, 
+        version=version, 
+        permissions=permissions, 
+        expires=expires,
+        auto.dedup.md5=collapse.md5.duplicates
+    )
 }
